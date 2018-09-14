7b2544083bddc5021d9d69d677f89dc6ce077e71
rafalpospiech <r.pospiech@yetiforce.com>
2018-09-14 14:00:09 +0200

    basic iniialization

---
+++ lib/Document/Document.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+/**
+ * Document class
+ *
+ * @package   YetiPDF\Document
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Rafal Pospiech <r.pospiech@yetiforce.com>
+ */
+
+namespace YetiPDF\Document;
+
+/**
+ * Class Document
+ */
+class Document
+{
+
+	protected $pages = [];
+
+	/**
+	 * Document constructor.
+	 */
+	public function __construct()
+	{
+
+	}
+
+}
--- lib/YetiPDF.php
+++ lib/YetiPDF.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 /**
  * YetiPDF class for generating pdf documents out of html
@@ -17,4 +18,26 @@ namespace YetiPDF;
  */
 class YetiPDF
 {
+	/**
+	 * Main document instance
+	 * @var \YetiPDF\Document\Document $document
+	 */
+	private $document;
+
+	/**
+	 * YetiPDF constructor.
+	 */
+	public function __construct()
+	{
+		$this->document = new \YetiPDF\Document\Document();
+	}
+
+	/**
+	 * Get document instance
+	 * @return \YetiPDF\Document\Document
+	 */
+	public function getDocument(): \YetiPDF\Document\Document
+	{
+		return $this->document;
+	}
 }
