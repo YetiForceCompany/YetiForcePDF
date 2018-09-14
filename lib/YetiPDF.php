1ee9d008e4d564c636e5b704e7f73177eb06a057
rafalpospiech <r.pospiech@yetiforce.com>
2018-09-14 13:30:31 +0200

    main class created

---
+++ lib/YetiPDF.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * YetiPDF class for generating pdf documents out of html
+ *
+ * @package   YetiPDF
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Rafal Pospiech <r.pospiech@yetiforce.com>
+ */
+
+namespace YetiPDF;
+
+/**
+ * Class YetiPDF
+ */
+class YetiPDF
+{
+}
