28347eaabb154d06d6c67081a69689e434f79327
rafalpospiech <r.pospiech@yetiforce.com>
2018-09-14 14:10:31 +0200

    Initial Page class

---
+++ lib/Document/Page.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+/**
+ * Page class
+ *
+ * @package   YetiPDF\Document
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Rafal Pospiech <r.pospiech@yetiforce.com>
+ */
+
+namespace YetiPDF\Document;
+
+/**
+ * Class Document
+ */
+class Page
+{
+	/**
+	 * Portrait page orientation
+	 */
+	const ORIENTATION_PORTRAIT = 'P';
+	/**
+	 * Landscape page orientation
+	 */
+	const ORIENTATION_LANDSCAPE = 'L';
+
+	/**
+	 * Current page format
+	 * @var string $format
+	 */
+	protected $format;
+	/**
+	 * Current page orientation
+	 * @var string $orientation
+	 */
+	protected $orientation;
+
+	/**
+	 * Document constructor.
+	 * @param string $format
+	 * @param string $orientation
+	 */
+	public function __construct(string $format = 'A4', string $orientation = self::ORIENTATION_PORTRAIT)
+	{
+		$this->format = $format;
+		$this->orientation = $orientation;
+	}
+
+}
