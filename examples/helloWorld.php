da74b9439243aad44e0a92344642596c8e1d1ece
rafalpospiech <r.pospiech@yetiforce.com>
2018-10-22 14:17:12 +0200

    do not output result to console

--- examples/helloWorld.php
+++ examples/helloWorld.php
@@ -3,5 +3,4 @@ $loader = require '../vendor/autoload.php';
 $document = (new YetiForcePDF\Document())->init();
 $document->loadHtml(file_get_contents('helloWorld.html'));
 $pdfFile = $document->render();
-echo $pdfFile;
 file_put_contents('helloWorld.pdf', $pdfFile);
