ecafd01848cd12fe31a496aff55154a4da5e6f73
rafalpospiech <r.pospiech@yetiforce.com>
2018-10-26 09:42:30 +0200

    additional table boxex added

---
+++ lib/Layout/TableColumnBox.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+/**
+ * TableColumnBox class
+ *
+ * @package   YetiForcePDF\Layout
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   MIT
+ * @author    Rafal Pospiech <r.pospiech@yetiforce.com>
+ */
+
+namespace YetiForcePDF\Layout;
+
+use \YetiForcePDF\Style\Style;
+use \YetiForcePDF\Html\Element;
+use \YetiForcePDF\Layout\Coordinates\Coordinates;
+use \YetiForcePDF\Layout\Coordinates\Offset;
+use \YetiForcePDF\Layout\Dimensions\BoxDimensions;
+
+/**
+ * Class TableColumnBox
+ */
+class TableColumnBox extends InlineBlockBox
+{
+
+}
