6925e3d9f0613d21ce0a7f376a621482a0c769c6
rafalpospiech <r.pospiech@yetiforce.com>
2018-12-11 11:05:33 +0100

    recurrent page dividing on block elements is kind of acceptable

--- lib/Layout/Coordinates/Coordinates.php
+++ lib/Layout/Coordinates/Coordinates.php
@@ -21,112 +21,111 @@ use \YetiForcePDF\Math;
 class Coordinates extends \YetiForcePDF\Base
 {
 
-    /**
-     * @var Box
-     */
-    protected $box;
-    /**
-     * Absolute X position inside html coordinate system
-     * @var string
-     */
-    protected $htmlX = '0';
-    /**
-     * Absolute Y position inside html coordinate system
-     * @var string
-     */
-    protected $htmlY = '0';
+	/**
+	 * @var Box
+	 */
+	protected $box;
+	/**
+	 * Absolute X position inside html coordinate system
+	 * @var string
+	 */
+	protected $htmlX = '0';
+	/**
+	 * Absolute Y position inside html coordinate system
+	 * @var string
+	 */
+	protected $htmlY = '0';
 
-    /**
-     * Set box
-     * @param \YetiForcePDF\Layout\Box $box
-     * @return $this
-     */
-    public function setBox(Box $box)
-    {
-        $this->box = $box;
-        return $this;
-    }
+	/**
+	 * Set box
+	 * @param \YetiForcePDF\Layout\Box $box
+	 * @return $this
+	 */
+	public function setBox(Box $box)
+	{
+		$this->box = $box;
+		return $this;
+	}
 
-    /**
-     * Get box
-     * @return \YetiForcePDF\Layout\Box
-     */
-    public function getBox()
-    {
-        return $this->box;
-    }
+	/**
+	 * Get box
+	 * @return \YetiForcePDF\Layout\Box
+	 */
+	public function getBox()
+	{
+		return $this->box;
+	}
 
-    /**
-     * Set absolute html coordinates x position
-     * @param float $x
-     * @return $this
-     */
-    public function setX(string $x)
-    {
-        $this->htmlX = $x;
-        return $this;
-    }
+	/**
+	 * Set absolute html coordinates x position
+	 * @param float $x
+	 * @return $this
+	 */
+	public function setX(string $x)
+	{
+		$this->htmlX = $x;
+		return $this;
+	}
 
-    /**
-     * Get html X
-     * @return string
-     */
-    public function getX()
-    {
-        return $this->htmlX;
-    }
+	/**
+	 * Get html X
+	 * @return string
+	 */
+	public function getX()
+	{
+		return $this->htmlX;
+	}
 
-    /**
-     * Get html Y
-     * @return string
-     */
-    public function getY()
-    {
-        return $this->htmlY;
-    }
+	/**
+	 * Get html Y
+	 * @return string
+	 */
+	public function getY()
+	{
+		return $this->htmlY;
+	}
 
-    /**
-     * Set absolute html coordinates y position
-     * @param string $y
-     * @return $this
-     */
-    public function setY(string $y)
-    {
-        $this->htmlY = $y;
-        return $this;
-    }
+	/**
+	 * Set absolute html coordinates y position
+	 * @param string $y
+	 * @return $this
+	 */
+	public function setY(string $y)
+	{
+		$this->htmlY = $y;
+		return $this;
+	}
 
 
-    /**
-     * Get pdf X coodrinates
-     * @return string
-     */
-    public function getPdfX()
-    {
-        return $this->htmlX;
-    }
+	/**
+	 * Get pdf X coodrinates
+	 * @return string
+	 */
+	public function getPdfX()
+	{
+		return $this->htmlX;
+	}
 
-    /**
-     * Convert html to pdf y
-     * @return string
-     */
-    public function getPdfY()
-    {
-        $height = $this->box->getDimensions()->getHeight();
-        $page = $this->document->getCurrentPage();
-        return Math::sub($page->getOuterDimensions()->getHeight(), Math::add($this->htmlY, $height));
-    }
+	/**
+	 * Convert html to pdf y
+	 * @return string
+	 */
+	public function getPdfY()
+	{
+		$height = $this->box->getDimensions()->getHeight();
+		$page = $this->document->getCurrentPage();
+		return Math::sub($page->getOuterDimensions()->getHeight(), Math::add($this->htmlY, $height));
+	}
 
-    /**
-     * Get end Y - position at the end of box
-     * @return string
-     */
-    public function getEndY()
-    {
-        $box = $this->getBox();
-        $height = $box->getDimensions()->getHeight();
-        $height = Math::add($height, $box->getStyle()->getRules('margin-bottom'));
-        return Math::add($height, $this->htmlY);
-    }
+	/**
+	 * Get end Y - position at the end of box
+	 * @return string
+	 */
+	public function getEndY()
+	{
+		$box = $this->getBox();
+		$height = $box->getDimensions()->getHeight();
+		return Math::add($height, $this->htmlY);
+	}
 
 }
